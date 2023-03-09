5a1555a47e7dd98426a832dfe81706d02932d173
mauroeag <116832653+mauroeag@users.noreply.github.com>
2023-03-10 00:21:46 +0100

    Update testMedia.py

--- testMedia.py
+++ testMedia.py
@@ -16,6 +16,7 @@ def test_add_Nota_Error_20():
 
 def test_add_Nota_Error_low():
     media = Media()
+    media.notas().clear()
     assert len(media.notas()) == 0
     with pytest.raises(ValueError):
         media.add(-1)
